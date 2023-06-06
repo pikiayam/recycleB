45829c552b069cae91f27aa24bb8794dbebd0b95
RecyclerViewA <92361044+pikiayam@users.noreply.github.com>
2023-06-06 10:56:24 +0700

    Initial commit

---
+++ app/src/test/java/com/vickyhadisaputra202102353/recycleb/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.vickyhadisaputra202102353.recycleb;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
